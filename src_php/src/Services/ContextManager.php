f67410b6ffcb1e81f8ec1b71fd6a4c4796ba2eb0
Denis Oliver Kropp <dok@directfb1.org>
2025-12-16 22:25:44 +0100

    PHP: cleanup (hotfix)

--- src_php/src/Services/ContextManager.php
+++ src_php/src/Services/ContextManager.php
@@ -30,11 +30,6 @@ class ContextManager
 
         return $this->qdrantClient->upsertVectors($this->collectionName, [
             [
-                'id' => uniqid(), // Note: Qdrant prefers UUIDs or integers. uniqid is string.
-                // Ideally we should use a UUID generator. For now, let's assume Qdrant accepts string IDs if configured, 
-                // but standard Qdrant expects UUID or Int. Let's use a simple hash for now or a proper UUID if we had a library.
-                // Let's use md5 hash of uniqid to get a 32-char hex string which is close to UUID but not quite.
-                // Actually, let's just use a random integer for simplicity in this demo, or a proper UUID v4 function.
                 'id' => $this->generateUuid(),
                 'vector' => $embedding,
                 'payload' => array_merge($metadata, ['text' => $text])
